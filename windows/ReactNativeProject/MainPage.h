c42b67f641c75ad6763fa33975c9eaa4fb1a568b
spiduso <hoang.dusan@gmail.com>
2024-10-03 12:27:38 +0200

    init

---
+++ windows/ReactNativeProject/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::ReactNativeProject::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::ReactNativeProject::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
